d0463151e8f8f4e1dc5eb9fad2c21267f9bb1d8b
sakhawat19 <sakhawat19@gmail.com>
2018-04-18 17:26:34 +0600

    test com 1

--- app/src/main/java/com/jewel_mahmud/www/pushup/MainActivity.java
+++ app/src/main/java/com/jewel_mahmud/www/pushup/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Updated", "Completed. Wohhie Mahmud");
+        Log.e("Updated", "Completed. Wohhie Mahmud qweqwe");
 
 
     }
