7276c33b6ffe18f624e98cb245d6c0528065dc75
wohhie <you@example.com>
2018-04-18 13:08:03 +0600

    Initial commit

---
+++ app/src/main/java/com/jewel_mahmud/www/pushup/MainActivity.java
@@ -0,0 +1,21 @@
+package com.jewel_mahmud.www.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private int i = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("Updated", "Completed.");
+
+    }
+
+    private void SampleMethod(int i){
+        i = i + 1;
+    }
+}
